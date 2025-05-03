26e4b771e609a8665360da4f01d184b6caba83af
Yahor Kenda <y.kenda@sdventures.com>
2025-05-03 18:25:39 +0200

    Implemented Dispatch domain service

---
+++ DeliveryApp.Core/Domain/Services/Dispatch/IDispatchService.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Services.Dispatch;
+
+public interface IDispatchService
+{
+    Result<Courier, Error> Dispatch(Order order, IReadOnlyCollection<Courier> couriers);
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Services/DispatchShould.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using DeliveryApp.Core.Domain.Services.Dispatch;
+using DeliveryApp.Core.Domain.SharedKernel;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Services;
+
+public class DispatchShould
+{
+    public static IEnumerable<object[]> GetInvalidDispatchPreconditions()
+    {
+        yield return [null, new List<Courier> { Courier.Create("Ivan", "car", 3, Location.Create(1, 1).Value).Value }];
+        yield return [Order.Create(Guid.NewGuid(), Location.Create(5, 5).Value).Value, null];
+        yield return [Order.Create(Guid.NewGuid(), Location.Create(5, 5).Value).Value, new List<Courier>()];
+        
+        var courier = Courier.Create("Ivan", "car", 3, Location.Create(1, 1).Value).Value;
+        var busyCourier = courier.SetBusy().Value;
+        yield return [Order.Create(Guid.NewGuid(), Location.Create(5, 5).Value).Value, new List<Courier> { busyCourier }];
+    }
+    
+    [Fact]
+    public void BeDoneWithCourierThatWillFasterReachToOrderLocation()
+    {
+        var courier1 = Courier.Create("Ivan", "car", 3, Location.Create(1, 1).Value);
+        var courier2 = Courier.Create("Petr", "bicycle", 2, Location.Create(2, 2).Value);
+        var courier3 = Courier.Create("Pavel", "pedestrian", 1, Location.Create(3, 3).Value);
+        
+        var order = Order.Create(Guid.NewGuid(), Location.Create(5, 5).Value);
+        
+        var dispatchService = new DispatchService();
+        var result = dispatchService.Dispatch(order.Value, [courier1.Value, courier2.Value, courier3.Value]);
+        
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(courier1.Value);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetInvalidDispatchPreconditions))]
+    public void ReturnErrorWhenParamsAreIncorrect(Order order, IReadOnlyCollection<Courier> couriers)
+    {
+        // Arrange
+        
+        // Act
+        var dispatchService = new DispatchService();
+        var result = dispatchService.Dispatch(order, couriers);
+        
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().NotBeNull();
+    }
+}
